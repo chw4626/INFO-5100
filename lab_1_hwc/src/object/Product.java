f65681d22206e2e33cf2565cab6b7593401a9fe7
chw4626 <664153604@qq.com>
2018-09-11 16:33:48 -0400

    9.10

---
+++ lab_1_hwc/src/object/Product.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package object;
+
+/**
+ *
+ * @author haowenchang
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String num;
+    private String descip;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getDescip() {
+        return descip;
+    }
+
+    public void setDescip(String descip) {
+        this.descip = descip;
+    }
+    
+}
